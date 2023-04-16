dfb990a317d41d13daf937a98c5d3b06f41060d7
Anubhav-Goyal01 <anubhavgoyal101@gmai.com>
2023-04-16 13:13:56 +0530

    Added Model Trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,97 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from sklearn.metrics import r2_score
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from xgboost import XGBRegressor
+from catboost import CatBoostRegressor
+from lightgbm import LGBMRegressor
+
+from src.exception import HousingException
+from src.logger import logging
+
+from src.utils import save_object, evaluate_models
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts', "model.pkl")
+
+
+
+class ModelTrainer:
+    def __init__(self) -> None:
+        self.model_trainer_config = ModelTrainerConfig()
+
+
+    def initiate_model_trainer(self, train_arr, test_arr):
+        try:
+            logging.info("Splitting train and test array")
+            X_train, y_train, X_test, Y_test = (
+                train_arr[:, :-1],
+                train_arr[:, -1],
+                test_arr[:, :-1],
+                test_arr[:, -1]
+            )
+
+            models = {
+                'xgboost' : XGBRegressor(),
+                'catboost' : CatBoostRegressor(verbose=0),
+                'lightgbm' : LGBMRegressor(),
+                'gradient boosting' : GradientBoostingRegressor(),
+                'random forest' : RandomForestRegressor(),
+            }
+
+
+            params={
+                "xgboost":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+
+                "catboost":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+
+                "lightgbm":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'max_depth': [6,8,10],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+
+                "gradient boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+
+                "random forest":{
+                    'n_estimators': [8,16,32,64,128,256]
+                }           
+            }
+
+            model_report:dict = evaluate_models(X_train, y_train, X_test, Y_test, models, params)
+            best_model_score = max(sorted(model_report.values()))
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise HousingException("No best Model Found")
+
+            logging.info("Model trained successfully")
+
+            save_model(
+                filepath = self.model_trainer_config.trained_model_file_path, 
+                obj = best_model,
+            )
+
+            predicted = best_model.predict(X_test)
+            r2_score = r2_score(y_test, predicted)
+            return r2_score
+
+
+        except Exception as e:
+            raise HousingException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,13 @@
 import os
-import pickle
 import sys
-from src.exception import HousingException
 
+import numpy as np 
+import pandas as pd
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import HousingException
 
 def save_object(file_path, obj):
     try:
@@ -12,6 +17,45 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            parameters = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,parameters,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise HousingException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise HousingException(e, sys)
